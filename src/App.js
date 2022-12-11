4a09e96fdf1d76880bf3a7921f559914b6237c98
Marko1310 <marko.cabo13@gmail.com>
2022-12-11 17:25:17 +0100

    Basic structure of the game, implemented logic for typing into typearea, starting countdown when button is clicked

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  // state for input text
+  const [text, setText] = useState("");
+
+  // state for number of words
+  const [wordCount, setWordCount] = useState(0);
+
+  // state/ref for timer
+  const [changeTime, setChangeTime] = useState(5);
+
+  // state for stert button clicked
+  const [isCounting, setIsCounting] = useState(false);
+
+  // function to split the string, filter words, count and update wordCount state
+  const typeWords = function (e) {
+    setText(e.target.value);
+    const wordsArray = text.split(" ").filter((word) => word.length > 0);
+    setWordCount(wordsArray.length);
+  };
+
+  useEffect(() => {
+    if (isCounting && changeTime > 0) {
+      lowerTheNumber();
+    } else if (changeTime === 0) {
+      console.log("end game");
+    }
+  }, [changeTime, isCounting]);
+
+  function lowerTheNumber() {
+    setTimeout(() => {
+      setChangeTime((changeTime) => changeTime - 1);
+      console.log(changeTime);
+    }, 1000);
+  }
+
+  function startGame() {
+    setIsCounting(true);
+  }
+
+  // const startTimer = function () {
+  //   setInterval(() => {
+  //     console.log(changeTime);
+  //     setChangeTime((prevchangeTime) => prevchangeTime--);
+  //     console.log(changeTime);
+  //     if (changeTime > 0) {
+  //       console.log("ne");
+  //     }
+  //   }, 1000);
+  // };
+
+  // console.log(changeTime.current);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main--container">
+      <h1>How fast can you type?</h1>
+      <textarea onChange={typeWords} />
+      <h4>Time Remaining: {changeTime.current}</h4>
+      <button onClick={startGame}>START</button>
+      <h1>Word count: {wordCount}</h1>
     </div>
   );
 }
